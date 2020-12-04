f1ec868e28144ade84bea0dedefbce6820c433ef
Alicia Weenum <weenum.alicia@gmail.com>
2020-12-04 01:55:18 -0500

    commit tree 7d7c3ff129a26669df0c37a72b624804fafcfb14

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,43 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    "Hello"
+  end
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    @articles = Article.new
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create({title:params["title"], content:params["content"]})
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+     erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article]) #whenever i pull from the hash, it becomes a symbol
+    redirect "/articles/#{@article.id}" #whenever I have a PATCH< i want to redirect it
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.delete
+    redirect "/articles/#{@article.id}"
   end
 end
